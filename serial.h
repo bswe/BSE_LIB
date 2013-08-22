4f72a800a0c907593a0d882cc01400f545cb56d1
Bill  Bollenbacher <billbollenba@yahoo.com>
2013-08-21 21:04:11 -0700

    Added new header comment

--- serial.h
+++ serial.h
@@ -1,10 +1,23 @@
-/*
- * serial.h
+/**** Serial USART library for Atmel ATmega328 *******
+ * 
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
  *
- * Author: Bollenbacher Software Engineering
- * Created: 7/14/13
- */ 
-
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ *
+ * @author Bollenbacher Software Engineering
+ * Created: 8/19/13
+ */
 
 #ifndef SERIAL_H_
 #define SERIAL_H_
